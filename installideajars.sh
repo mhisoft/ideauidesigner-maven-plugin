276ad348fd91c08c4f1bdedb44da0e7b932550b0
I831964 <zhongyuan.xue@sap.com>
2019-01-18 15:14:18 -0500

    added the dependency intellij jars

--- installideajars.sh
+++ installideajars.sh
@@ -1,3 +1,3 @@
-mvn install:install-file -Dfile=../idea-lib/javac2.jar -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.0 -Dpackaging=jar
-mvn install:install-file -Dfile=../idea-lib/forms_rt.jar -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.0 -Dpackaging=jar
-mvn install:install-file -Dfile=../idea-lib/asm-all.jar -DgroupId=com.intellij -DartifactId=asm-all -Dversion=14.0 -Dpackaging=jar
+mvn install:install-file -Dfile=./idea-lib/javac2.jar -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.0 -Dpackaging=jar
+mvn install:install-file -Dfile=./idea-lib/forms_rt.jar -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.0 -Dpackaging=jar
+mvn install:install-file -Dfile=./idea-lib/asm-all.jar -DgroupId=com.intellij -DartifactId=asm-all -Dversion=14.0 -Dpackaging=jar
