2f46744177529da9f613be890156dba02ab059e5
MHISoft <tony.zxue@gmail.com>
2020-11-07 13:11:49 -0500

    updated the maven repo URL; added the three intelliJ jars. updated unstructions.

---
+++ installideajars.cmd
@@ -0,0 +1,4 @@
+
+mvn install:install-file -Dfile="javac2.jar" -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.0 -Dpackaging=jar
+mvn install:install-file -Dfile="forms_rt.jar" -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.0 -Dpackaging=jar
+mvn install:install-file -Dfile="asm-all-8.0.1.jar".jar -DgroupId=com.intellij -DartifactId=asm-all -Dversion=14.0 -Dpackaging=jar
