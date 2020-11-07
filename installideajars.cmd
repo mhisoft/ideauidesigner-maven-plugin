99f9a9e16768596bf7055604dfb056c676741a47
MHISoft <tony.zxue@gmail.com>
2020-11-07 13:12:04 -0500

    update

--- installideajars.cmd
+++ installideajars.cmd
@@ -1,4 +1,3 @@
-
-mvn install:install-file -Dfile="javac2.jar" -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.0 -Dpackaging=jar
-mvn install:install-file -Dfile="forms_rt.jar" -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.0 -Dpackaging=jar
-mvn install:install-file -Dfile="asm-all-8.0.1.jar".jar -DgroupId=com.intellij -DartifactId=asm-all -Dversion=14.0 -Dpackaging=jar
+cmd /c mvn install:install-file -Dfile="javac2.jar" -DgroupId=com.intellij -DartifactId=javac2 -Dversion=14.0 -Dpackaging=jar 
+cmd /c mvn install:install-file -Dfile="forms_rt.jar" -DgroupId=com.intellij -DartifactId=forms_rt -Dversion=14.0 -Dpackaging=jar 
+cmd /c  mvn install:install-file -Dfile="asm-all-8.0.1.jar" -DgroupId=com.intellij -DartifactId=asm-all -Dversion=14.0 -Dpackaging=jar 
